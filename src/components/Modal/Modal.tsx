04ba290df0c148b091686e5c449ddeee06685dff
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-11-05 15:04:09 +0100

    update to various files

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,19 +1,26 @@
 interface ModalProps {
-    show: boolean;
-    onClose: () => void;
-    children: React.ReactNode;
+  show: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
 }
 
 const Modal = ({ show, onClose, children }: ModalProps) => {
-    if (!show) return null;
+  if (!show) return null;
 
-    return (
-        <div className="modal-backdrop">
-            <div className="modal-content">
-                {children}
-            </div>
-        </div>
-    )
-}
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    // Check if the modal content itself (not its children) was clicked
+    if ((event.target as HTMLElement).classList.contains('modal-backdrop')) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className="modal-backdrop" onClick={handleBackdropClick}>
+      <div className="modal-content">
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,23 +1,49 @@
+import { useState, useEffect } from 'react';
 import NavLinks from './NavLinks';
+import Modal from '../Modal/Modal';
 
 import hamburger from '@assets/images/icon-hamburger.svg';
+import closeIcon from '@assets/images/icon-close.svg';
 import logo from '@assets/images/logo.svg';
 import Button from '../Button/Button';
 
 import './navigation.css';
 
-export default function Navigation() {
+const Navigation = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        isModalOpen &&
+        event.target instanceof Node &&
+        !document.querySelector('.modal-content')?.contains(event.target)
+      ) {
+        setModalOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [isModalOpen]);
+
   return (
     <div className="nav-container">
       <div className="logo">
         <img src={logo} className="logo-image" />
       </div>
 
-      <div className="hamburger">
+      <div className="hamburger" onClick={toggleModal}>
         <img
-          src={hamburger}
+          src={isModalOpen ? closeIcon : hamburger}
           className="icon-hamburger"
-          width={25}
+          width={isModalOpen ? 20 : 25}
           height={15}
           alt=""
         />
@@ -26,10 +52,18 @@ export default function Navigation() {
       <NavLinks />
       <Button
         onClick={() => console.log('clicked')}
-        type='button'
-        className='btn btn-top'
-        children='Request Invite'
+        type="button"
+        className="btn btn-top"
+        children="Request Invite"
       />
+
+      <Modal show={isModalOpen} onClose={toggleModal}>
+        {/* Modal Content Here */}
+        <NavLinks />
+        {/* You might want to display the nav links in the modal too */}
+      </Modal>
     </div>
   );
 }
+
+export default Navigation;
